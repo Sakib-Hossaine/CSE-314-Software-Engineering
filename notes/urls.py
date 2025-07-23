ef77c135706f669baab40537716a19b24ea3d4ef
Your Name <you@example.com>
2025-07-23 21:04:36 +0600

    Starting Project

--- notes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("notes/", views.notes, name="notes"),
-    path("notes/download/<int:note_id>/", views.download_note, name="download_note"),
-    path("notes/report/<int:note_id>/", views.report_note, name="report_note"),
-]
--- routine/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import routine
-
-
-
-urlpatterns = [
-   path("routine/", routine, name="routine"),
-]
